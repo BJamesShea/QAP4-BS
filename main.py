c09b66b161ab854ef5f61c77b3524e744b6c8bf4
Brandon Shea <brandon.shea1994@icloud.com>
2024-03-14 12:33:59 -0230

    Project start

---
+++ main.py
@@ -0,0 +1,22 @@
+# Description:
+# Author:
+# Date(s):
+
+# Define required libraries.
+
+# Define program constants.
+
+# Define program functions.
+
+
+# Main program starts here.
+while True:
+
+    # Gather user inputs.
+
+    # Perform required calculations.
+
+    # Display results
+    
+
+# Any housekeeping duties at the end of the program
