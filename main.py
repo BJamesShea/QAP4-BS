fb4dbc6e3aad74a59ada1d88d8395aa47c7764bc
Brandon Shea <brandon.shea1994@icloud.com>
2024-03-17 00:13:22 -0230

    Add def.dat and initialize values

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-# Description: QAP# 4
+# Description: QAP# 4 - One stop Insruance Company
 # Author: Brandon Shea
-# Date(s): March 14th 2024
+# Date(s): March 14th 2024 - 
 
 # Define required libraries.
+import FormatValues as FV
 
 # Define program constants.
+f = open('def.dat', 'r')
+POLICY_NUM = int(f.readline())
+BASIC_PREM = float(f.readline())
+ADD_DISCOUNT = float(f.readline())
+EXTRA_LIAB_COV = float(f.readline())
+GLASS_COV = float(f.readline())
+LOANER_CAR = float(f.readline())
+HST_RATE = float(f.readline())
+PROC_FEE = float(f.readline())
+f.close()
+
 
 # Define program functions.
 
