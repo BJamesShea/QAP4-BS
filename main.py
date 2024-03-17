67ac51be5215896510f5a417b651a016d925a860
Brandon Shea <brandon.shea1994@icloud.com>
2024-03-17 17:55:51 -0230

    Working on payment info validations.

--- main.py
+++ main.py
@@ -27,13 +27,12 @@ curDate = datetime.datetime.now()
 
 # Main program starts here.
        
-    # Gather user inputs.
-
-
+# Gather user inputs.
 
 def isValidName(name):
         ALLOWED_CHAR_SET = set("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz-.'1234567890 ")
         return set(name).issubset(ALLOWED_CHAR_SET)
+
 
 def customerInfo():
         while True:
@@ -86,7 +85,7 @@ def customerInfo():
             print("Error - Please only use letters, numbers, hyphens, apostrophes, and/or periods.")
 
         while True:
-            # Custome phone number
+            # Customer phone number
             phoneNum = input("Enter the customers phone number (###-###-####): ")
             if isValidName(phoneNum):
                 break
@@ -95,8 +94,93 @@ def customerInfo():
         # Returns
         return custFirstName, custLastName, custStrAdd, custCity, prov.upper(), postCode, phoneNum
 
+def vehicleInfo():
+        while True:
+            # Number of cars being insured
+            numCars = input("Enter the amount of cars to be insured. ")
+            if numCars.isdigit():
+                numCars = int(numCars)
+                break
+            print("Error - Please enter a valid number.")
+
+        while True:
+            # Extra Liability Coverage
+            extraLiab = input("Does the customer want Extra Liability Coverage? (Y for yes / N for no): ").upper()
+            if extraLiab in ["Y", "N"]:
+                break
+            print("Please enter Y or N.")
+            
+        if extraLiab == "Y":
+             print("Customer has opted for Extra Liability Coverage!")
+        else:
+            print("Customer has opted not to have Extra Liability Coverage.")
+
+        while True:
+            # Optional Glass Coverage
+            glassCoverage = input("Does the customer want Optional Glass Coverage? (Y for yes / N for no): ").upper()
+            if glassCoverage in ["Y", "N"]:
+                break
+            print("Please enter Y or N.")
+
+        if glassCoverage == "Y":
+             print("Customer has opted for Optional Glass Coverage!")
+        else:
+            print("Customer has opted not to have Optional Glass Coverage.")
+
+
+        while True:
+            # Optional Loaner Car
+            loanerCar = input("Does the customer want a Loaner Car? (Y for yes / N for no): ").upper()
+            if loanerCar in ["Y", "N"]:
+                break
+            print("Please enter Y or N.")
+
+        if loanerCar == "Y":
+             print("Customer has opted for a Loaner Car!")
+        else:
+            print("Customer has opted not to have a Loaner Car.")
+
+            # Returns
+        return numCars, extraLiab, glassCoverage, loanerCar
+
+
+def paymentInfo():
+        paymentMethodLst = ["Full", "Monthly", "Down Pay"]
+        while True:
+            # Paying in monthly or full amount
+            paymentMethod = input("How would the customer like to pay? (Full, Monthly, or Down pay.): ").title()
+            if paymentMethod not in paymentMethodLst:
+                print("Error - Invalid payment type.")
+            elif paymentMethod == "Down Pay":
+                while True:
+                    downPaymentStr = input("How much would the customer like to put down as a down payment? ")
+                    try:
+                        downPayment = float(downPaymentStr)
+                        break
+                    except ValueError:
+                        print("Error - Please enter a valid numerical value.")
+                break
+            else:
+                break
+        return paymentMethod, downPayment
+
+
+
     # Perform required calculations.
-customerData = customerInfo()
+
+
+
+
+
+
+
+
+
+# Gather inputs
+while True:
+    customerData = customerInfo()
+    insuranceInfo = vehicleInfo()
+    paymentInformation = paymentInfo()
 
     # Display results
     
